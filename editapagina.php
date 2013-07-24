77ef2daa3e6899d87309e691e94f0905dc490145
sfcbbi <sfc@bbi.net.br>
2013-07-24 18:41:11 -0300

    Arquivos de imagens

---
+++ editapagina.php
@@ -0,0 +1,86 @@
+<?php 
+// A sessão precisa ser iniciada em cada página diferente
+if (!isset($_SESSION)) session_start();
+
+// Verifica se não há a variável da sessão que identifica o usuário
+if (!isset($_SESSION['cod_adm'])) {
+	// Destrói a sessão por segurança
+	session_destroy();
+	// Redireciona o visitante de volta pro login
+	header("Location: loginadm.php"); exit;
+}
+require_once('Connections/conexao.php'); ?>
+<?php
+if (!function_exists("GetSQLValueString")) {
+  function GetSQLValueString($theValue, $theType, $theDefinedValue = "", $theNotDefinedValue = "") 
+  {
+    if (PHP_VERSION < 6) {
+      $theValue = get_magic_quotes_gpc() ? stripslashes($theValue) : $theValue;
+    }
+
+    $theValue = function_exists("mysql_real_escape_string") ? mysql_real_escape_string($theValue) : mysql_escape_string($theValue);
+
+    switch ($theType) {
+      case "text":
+      $theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
+      break;    
+      case "long":
+      case "int":
+      $theValue = ($theValue != "") ? intval($theValue) : "NULL";
+      break;
+      case "double":
+      $theValue = ($theValue != "") ? doubleval($theValue) : "NULL";
+      break;
+      case "date":
+      $theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
+      break;
+      case "defined":
+      $theValue = ($theValue != "") ? $theDefinedValue : $theNotDefinedValue;
+      break;
+    }
+    return $theValue;
+  }
+}
+
+$editFormAction = $_SERVER['PHP_SELF'];
+if (isset($_SERVER['QUERY_STRING'])) {
+  $editFormAction .= "?" . htmlentities($_SERVER['QUERY_STRING']);
+}
+
+if ((isset($_POST["MM_update"])) && ($_POST["MM_update"] == "form")) {
+  $updateSQL = sprintf("UPDATE paginas SET cod_cliente=%s, nome_Pagina=%s, url_Pagina=%s, legenda=%s, imgPagina=%s WHERE cod_pagina=%s",
+   GetSQLValueString($_POST['codcl'], "int"),
+   GetSQLValueString($_POST['nome'], "text"),
+   GetSQLValueString($_POST['url'], "text"),
+   GetSQLValueString($_POST['legenda'], "text"),
+   GetSQLValueString($_POST['img'], "text"),
+   GetSQLValueString($_POST['codPagina'], "int"));
+
+  mysql_select_db($database_conexao, $conexao);
+  $Result1 = mysql_query($updateSQL, $conexao) or die(mysql_error());
+
+  $updateGoTo = "editapaginas.php";
+  if (isset($_SERVER['QUERY_STRING'])) {
+    $updateGoTo .= (strpos($updateGoTo, '?')) ? "&" : "?";
+    $updateGoTo .= $_SERVER['QUERY_STRING'];
+  }
+  header(sprintf("Location: %s", $updateGoTo));
+}
+
+mysql_select_db($database_conexao, $conexao);
+$query_Recordset1 = "SELECT * FROM clientes";
+$Recordset1 = mysql_query($query_Recordset1, $conexao) or die(mysql_error());
+$row_Recordset1 = mysql_fetch_assoc($Recordset1);
+$totalRows_Recordset1 = mysql_num_rows($Recordset1);
+
+$colname_Recordset2 = "-1";
+if (isset($_GET['cod'])) {
+  $colname_Recordset2 = $_GET['cod'];
+}
+mysql_select_db($database_conexao, $conexao);
+$query_Recordset2 = sprintf("SELECT * FROM paginas WHERE cod_pagina = %s", GetSQLValueString($colname_Recordset2, "int"));
+$Recordset2 = mysql_query($query_Recordset2, $conexao) or die(mysql_error());
+$row_Recordset2 = mysql_fetch_assoc($Recordset2);
+$totalRows_Recordset2 = mysql_num_rows($Recordset2);
+header("Location: adm.php?tab=5");
+?>
