7d3fbc5ea302cfef38c090869b93ae9afefea59e
sfcbbi <sfc@bbi.net.br>
2013-08-01 18:38:31 -0300

    Resolvendo JS

--- tableau_trusted.php
+++ tableau_trusted.php
@@ -30,7 +30,7 @@ Function get_trusted_ticket($wgserver, $user, $remote_addr) {
     'client_ip' => $remote_addr
   );
 
-  return https_parse_message(https_post_fields("https://$wgserver/trusted", $params))->body;
+  return http_parse_message(https_post_fields("https://$wgserver/trusted", $params))->body;
 }
 
 ?>
--- tableau_trusted2.php
+++ tableau_trusted2.php
@@ -32,7 +32,7 @@ function get_trusted_ticket($wgserver, $user, $remote_addr) {
     'client_ip' => $remote_addr
   );
 
-  return https_parse_message(https_post_fields("https://$wgserver/trusted", $params))->body;
+  return http_parse_message(https_post_fields("https://$wgserver/trusted", $params))->body;
 }
 
 ?>
